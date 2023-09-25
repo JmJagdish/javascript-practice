fd2ccdb3a6d08e909a18da63f878118928e2db59
Jagdish Mahanta <43695894+JmJagdish@users.noreply.github.com>
2023-09-25 07:49:42 +0000

    learn dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,4 +3,9 @@ let accountEmail = "jagdish456@gmail.com";
 var accountPassword = "12345";
 accountCity = "Bangalore";
 
-console.table(accountId, accountEmail, accountPassword, accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+/* 
+Prefer not to use var
+because of issue in block scope and functionl scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello");
+// alert( 3 + 3 ); // we are using node.js, not browser
+
+let name = "jagdish";
+let age = 25;
+let isLogIn = false;
+
+/*              primitive datatypes                */
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
