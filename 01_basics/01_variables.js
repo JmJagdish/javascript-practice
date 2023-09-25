016caef0f0ea68ae2970d7c43739bf667367023b
Jagdish Mahanta <43695894+JmJagdish@users.noreply.github.com>
2023-09-25 07:11:17 +0000

    font size upadated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553;
+let accountEmail = "jagdish456@gmail.com";
+var accountPassword = "12345";
+accountCity = "Bangalore";
+
+console.table(accountId, accountEmail, accountPassword, accountCity);
